eae279299c441aa12998065206bede9d50f56197
azartechnet <azartechnet@gmail.com>
2025-01-17 12:53:00 +0530

    updated

---
+++ NodeJs/nodep1.js
@@ -0,0 +1,6 @@
+var h1=require('http')
+h1.createServer(function(req,res){
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.write('<h1>HelloWorld</h1>')
+}).listen(8081)
+console.log("Server is Running...")
